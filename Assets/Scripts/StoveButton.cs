94517115c3bfa1e2f35230db79cd00fb06f32ba1
pablohd10 <phidalgod10@gmail.com>
2025-05-11 11:42:51 +0200

    botones stove funcionando

--- Assets/Scripts/StoveButton.cs
+++ Assets/Scripts/StoveButton.cs
@@ -1,21 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
+[RequireComponent(typeof(UnityEngine.XR.Interaction.Toolkit.Interactables.XRSimpleInteractable))]
 public class StoveButton : MonoBehaviour
 {
-    public StoveManager stove;
-    public AudioClip clickSound;
-    public bool isGreenButton = true; // Cambia esto según sea el verde o rojo
+    public StoveManager stove; // Referencia al script del StoveManager
+    public AudioClip clickSound; // Sonido que se reproducirá al presionar el botón
+    public bool isGreenButton = true; // Indica si el botón es verde (encender) o rojo (apagar)
 
-    public void OnButtonPressed()
+    private UnityEngine.XR.Interaction.Toolkit.Interactables.XRSimpleInteractable button; // Referencia al componente XRSimpleInteractable
+
+    private void Awake()
     {
+        // Obtiene el componente XRSimpleInteractable del GameObject
+        button = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRSimpleInteractable>();
+
+        // Agrega un listener al evento selectEntered, que se activa cuando el usuario interactúa con el botón
+        button.selectEntered.AddListener(OnButtonPressed);
+    }
+
+    private void OnDestroy()
+    {
+        // Elimina el listener del evento selectEntered cuando el objeto se destruye, para evitar errores
+        button.selectEntered.RemoveListener(OnButtonPressed);
+    }
+
+    private void OnButtonPressed(SelectEnterEventArgs args)
+    {
+        // Reproducir el sonido al presionar el botón
         if (clickSound != null)
         {
-            // Reproducir el sonido en el AudioSource adjunto al objeto
             AudioSource.PlayClipAtPoint(clickSound, transform.position);
         }
 
+        // Llama al método correspondiente según el tipo de botón
         if (isGreenButton)
             stove.TurnOn();
         else
